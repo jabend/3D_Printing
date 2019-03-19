afcacc69f0c18224a1e2afc3a2068145deb58480
Jon Abend <jabend@users.noreply.github.com>
2019-03-18 22:04:14 -0500

    Add files via upload

---
+++ addLinesTotalOnDisplay.py
@@ -0,0 +1,59 @@
+# Cura PostProcessingPlugin
+# Author:   Amanda de Castilho
+# Date:     August 28, 2018
+# Modified: Jon Abend
+# Date:     March 18, 2019
+
+# Description:  This plugin inserts a line at the start of each layer,
+#               M117 - displays the filename and layer height to the LCD
+#               Alternatively, user can override the filename to display alt text + layer height
+
+from ..Script import Script
+from UM.Application import Application
+import re
+
+class addLinesTotalOnDisplay(Script):
+    def __init__(self):
+        super().__init__()
+
+    def getSettingDataString(self):
+        return """{
+            "name": "Layer Info Display",
+            "key": "LayerInfoLCD",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "name":
+                {
+                    "label": "text to display:",
+                    "description": "By default the current filename will be displayed on the LCD. Enter text here to override the filename and display something else.",
+                    "type": "str",
+                    "default_value": ""
+                }
+            }
+        }"""
+
+    def execute(self, data):
+        if self.getSettingValueByKey("name") != "":
+            name = self.getSettingValueByKey("name")
+        else:
+            name = Application.getInstance().getPrintInformation().jobName
+        lcd_text = "M117 " + name + " layer "
+        i = 0
+        line_count = ""
+        for layer in data:
+            display_text = lcd_text + str(i)
+            layer_index = data.index(layer)
+            lines = layer.split("\n")
+            for line in lines:
+                if "COUNT:" in line.upper():
+                    line_count = '/' + re.sub(r'^.*COUNT: *',r'',line)
+                if line.startswith(";LAYER:"):
+                    line_index = lines.index(line)
+                    lines.insert(line_index + 1, display_text + line_count)
+                    i += 1
+            final_lines = "\n".join(lines)
+            data[layer_index] = final_lines
+
+        return data
